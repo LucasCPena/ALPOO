c9a4a559a45f3780c50f2a3e63762ccb4fabc31c
Lucas <lucascicalap@hotmail.com>
2021-08-17 19:55:17 -0300

    att

---
+++ ALPOO_2.java
@@ -0,0 +1,10 @@
+import java.awt.Frame;
+
+public class ALPOO_2 {
+      public static void main(String[] args) {
+          
+
+         Frame t01 = new Frame(); // Cria objeto janela
+          t01.setTitle("Tela01-Utilizando AWT");
+      }
+}
