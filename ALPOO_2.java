b38a4e44082e1a5f5be86c793715925dca79a47b
Lucas <lucascicalap@hotmail.com>
2021-08-17 21:00:13 -0300

    att

--- ALPOO_2.java
+++ ALPOO_2.java
@@ -1,10 +1,21 @@
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ALPOO_2 {
-      public static void main(String[] args) {
-          
+      public static void main(String[] args) {        
 
          Frame t01 = new Frame(); // Cria objeto janela
-          t01.setTitle("Tela01-Utilizando AWT");
+          t01.setTitle("Tela01-Utilizando AWT");// titulo da janela
+          t01.setSize(500, 300); //tamanho da janela horizontal x vertical
+          t01.setVisible(true);// deixa a janel visivel
+          //fechamento da janela
+          t01.addWindowListener(new WindowAdapter(){
+              public void windowClosing(WindowEvent e){
+                  System.exit(0);
+              }
+
+            });
+
       }
 }
